ab377fbbd32f3ca838cb68c3ae4af76eb56c463e
hbarwick <hallambarwick@hotmail.com>
2022-05-07 08:13:54 +0100

    Add project files.

---
+++ LineCondenser.ViewModel/TextConvert.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace TextConverter.ViewModel
+{
+    public class TextConvertViewModel : INotifyPropertyChanged
+    {
+        private string? LeftDelimiter { get; set; }
+        private string? RightDelimiter { get; set; }
+        private string? Separator { get; set; }
+        private string? LineStart { get; set; }
+        private string? LineEnd { get; set; }
+        private string? TextEntry { get; set; }
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string ProcessText()
+        {
+            string[] lines = TextEntry.Split
+
+            string result;
+
+
+            return result;
+        }
+    }
+}
